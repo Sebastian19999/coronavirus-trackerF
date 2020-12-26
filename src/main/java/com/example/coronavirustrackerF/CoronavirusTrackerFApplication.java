80390830a03bb79508ffeabd75b84efa4dbda1a0
sebygalan <seby.galan@gmail.com>
2020-12-26 20:36:26 +0200

    Initial commit

---
+++ src/main/java/com/example/coronavirustrackerF/CoronavirusTrackerFApplication.java
@@ -0,0 +1,15 @@
+package com.example.coronavirustrackerF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CoronavirusTrackerFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerFApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coronavirustrackerF/CoronavirusTrackerFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coronavirustrackerF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
